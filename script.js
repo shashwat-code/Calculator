ecf1c6b6b2da4ac15397c146207bb576b5f1056c
shashwat-code <91280518+shashwat-code@users.noreply.github.com>
2022-01-15 11:58:27 +0530

    final changes

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
 let ans=""
-let num1=""
-let num2=""
 
-function getDigit(id){
-    console.log(document.getElementById(id).textContent)
+function getDigit(id,val){
+    if(val==1){
+        ans+=document.getElementById(id).textContent
+    }else{
+        ans+=document.getElementById(id).value
+    }
+    
+    console.log(ans)
 }
 
-function displayResult(num){
-    let display = document.getElementById("output")
-    display.innerHTML = num
+function displayResult(){
+    let display = document.getElementById("input")
+    display.innerHTML = checkAnswer()
 }
-displayResult(23)
+
+function checkAnswer(){
+    let result = eval(ans);
+    if(result==-Infinity){
+        result=-1*result
+    }
+    return result.toString()
+}
+
+function getResult(){
+    displayResult(checkAnswer())
+}
+
+function clearInput(){
+    ans=""
+    let display = document.getElementById("input")
+    display.innerHTML = "0"
+    console.log("this is it: "+ans)
+}
