912b9447771e0cc78be1f7d04a44dccd5f457691
shashwat-code <91280518+shashwat-code@users.noreply.github.com>
2022-01-15 12:25:06 +0530

    adding modulus

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 let ans=""
-
+let num=""
 function getDigit(id,val){
     if(val==1){
         ans+=document.getElementById(id).textContent
+        num+=document.getElementById(id).textContent
+        displayResult(num)
     }else{
         ans+=document.getElementById(id).value
+        num=""
     }
 }
 
 function displayResult(res){
     let display = document.getElementById("input")
     display.innerHTML = res
+
 }
 
 function checkAnswer(){
@@ -22,10 +26,16 @@ function checkAnswer(){
 }
 
 function getResult(){
+    num=""
     displayResult(checkAnswer())
 }
 
 function clearInput(){
     ans=""
+    num=""
     displayResult("0")
+}
+
+function print(){
+    console.log(num+" "+ ans)
 }
