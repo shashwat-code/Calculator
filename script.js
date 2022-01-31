143908aa3172e0d503fae8dc0a77912257038270
shashwat-code <91280518+shashwat-code@users.noreply.github.com>
2022-01-31 11:35:20 +0530

    final changes

--- script.js
+++ script.js
@@ -1,41 +1,11 @@
-let ans=""
-let num=""
-function getDigit(id,val){
-    if(val==1){
-        ans+=document.getElementById(id).textContent
-        num+=document.getElementById(id).textContent
-        displayResult(num)
-    }else{
-        ans+=document.getElementById(id).value
-        num=""
-    }
+function display(val){
+    document.getElementById("input").value+=val;
 }
-
-function displayResult(res){
-    let display = document.getElementById("input")
-    display.innerHTML = res
-
+function solve(){
+    let x = document.getElementById("input").value;
+    let y =eval(x);
+    document.getElementById("input").value=y;
 }
-
-function checkAnswer(){
-    let result = eval(ans);
-    if(result==-Infinity){
-        result=-1*result
-    }
-    return result.toString()
-}
-
-function getResult(){
-    num=""
-    displayResult(checkAnswer())
-}
-
-function clearInput(){
-    ans=""
-    num=""
-    displayResult("0")
-}
-
-function print(){
-    console.log(num+" "+ ans)
+function inputClr(){
+    document.getElementById("input").value="";
 }
